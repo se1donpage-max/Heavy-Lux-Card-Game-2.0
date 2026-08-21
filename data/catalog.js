05eae407c67dfdefa6de5233ffda1525d245a70f
neveshestvo <se1don.page@gmail.com>
2026-08-22 00:22:23 +0300

    Update catalog.js

--- data/catalog.js
+++ data/catalog.js
@@ -1,5 +1,20 @@
 "use strict";
 
+/*
+=========================================================
+HEAVY LUX CARD
+CATALOG
+BASE V3
+=========================================================
+*/
+
+
+/*
+=========================================================
+VEHICLES
+=========================================================
+*/
+
 const VEHICLES = Object.freeze(
   [
     ["BMW", "3 Series", 65000],
@@ -12,6 +27,7 @@ const VEHICLES = Object.freeze(
     ["Skoda", "Kodiaq", 50000],
     ["Lexus", "ES", 85000],
     ["Volvo", "XC60", 78000],
+
     ["BMW", "5 Series", 110000],
     ["Mercedes-Benz", "E-Class", 115000],
     ["Audi", "A6", 108000],
@@ -30,18 +46,28 @@ const VEHICLES = Object.freeze(
   }))
 );
 
+
+/*
+=========================================================
+EXCLUSIVE VEHICLES
+=========================================================
+*/
+
 const EXCLUSIVE = [
   ["BMW", "M5 Competition", 280000, ""],
   ["Mercedes-Benz", "AMG GT 63 S", 320000, ""],
   ["Audi", "RS Q8", 290000, ""],
   ["Porsche", "911 Turbo S", 420000, ""],
+
   ["BMW", "M8 Competition MANSORY", 430000, "MANSORY"],
   ["Mercedes-Benz", "G 63 BRABUS", 520000, "BRABUS"],
   ["Porsche", "Cayenne Turbo HAMANN", 390000, "HAMANN"],
   ["Audi", "RS7 MANSORY", 410000, "MANSORY"],
+
   ["Lamborghini", "Huracán", 650000, ""],
   ["Ferrari", "488 GTB", 720000, ""],
   ["McLaren", "720S", 700000, ""],
+
   ["Porsche", "918 Spyder", 1800000, ""],
   ["Lamborghini", "Revuelto", 1200000, ""],
   ["Ferrari", "SF90 Stradale", 1300000, ""],
@@ -54,16 +80,26 @@ const EXCLUSIVE = [
   tuning,
 }));
 
+
+/*
+=========================================================
+PROPERTY
+=========================================================
+*/
+
 const PROPERTY = [
   ["lux", "Heavy Residence", "45000"],
   ["lux", "Skyline Apartment", "70000"],
   ["lux", "Black House", "95000"],
+
   ["absolute", "Absolute Penthouse", "160000"],
   ["absolute", "Royal Residence", "220000"],
   ["absolute", "Grand Villa", "300000"],
+
   ["legend", "Legend Estate", "500000"],
   ["legend", "Imperial Mansion", "750000"],
   ["legend", "Golden Coast Villa", "1000000"],
+
   ["brutal", "Brutal Palace", "1600000"],
   ["brutal", "Black Crown Estate", "2500000"],
 ].map(([tier, name, price], i) => ({
@@ -73,12 +109,27 @@ const PROPERTY = [
   price: Number(price),
 }));
 
+
+/*
+=========================================================
+PROPERTY COLORS
+=========================================================
+*/
+
 const PROPERTY_COLORS = Object.freeze({
   lux: "#79a9ff",
   absolute: "#b98cff",
   legend: "#ffd15c",
   brutal: "#ff5f6d",
 });
+
+
+/*
+=========================================================
+QUICK PHRASES
+=========================================================
+*/
+
 const QUICK_PHRASES = Object.freeze([
   "Спасибо за игру!",
   "Хорошей игры!",
@@ -86,16 +137,64 @@ const QUICK_PHRASES = Object.freeze([
   "Скорее!",
 ]);
 
+
+/*
+=========================================================
+BEAUTIFUL NUMBER PRICE
+=========================================================
+*/
+
 function numberPrice(value) {
   const digits = value.match(/\d+/)?.[0] || "";
   const letters = value.match(/[А-ЯЁ]/gi)?.[0] || "";
+
   let score = 1000;
-  if (/([0-9])\1\1/.test(digits)) score += 12000;
-  if (/777|888|999|001|007/.test(digits)) score += 8000;
-  if (/([А-ЯЁ])\1\1/.test(letters)) score += 10000;
-  if (/^[АУЕО]/i.test(value)) score += 1500;
+
+  /*
+  Triple identical digits:
+  111
+  222
+  777
+  etc.
+  */
+
+  if (/([0-9])\1\1/.test(digits)) {
+    score += 12000;
+  }
+
+  /*
+  Special combinations
+  */
+
+  if (/777|888|999|001|007/.test(digits)) {
+    score += 8000;
+  }
+
+  /*
+  Triple identical letters
+  */
+
+  if (/([А-ЯЁ])\1\1/.test(letters)) {
+    score += 10000;
+  }
+
+  /*
+  Premium first letters
+  */
+
+  if (/^[АУЕО]/i.test(value)) {
+    score += 1500;
+  }
+
   return score;
 }
+
+
+/*
+=========================================================
+BEAUTIFUL LICENSE PLATES
+=========================================================
+*/
 
 const BEAUTIFUL_NUMBERS = [
   "А111АА77",
@@ -107,6 +206,7 @@ const BEAUTIFUL_NUMBERS = [
   "А777АА77",
   "А888АА77",
   "А999АА77",
+
   "У111УУ77",
   "У222УУ77",
   "У333УУ77",
@@ -116,6 +216,7 @@ const BEAUTIFUL_NUMBERS = [
   "У777УУ77",
   "У888УУ77",
   "У999УУ77",
+
   "Е111ЕЕ77",
   "Е222ЕЕ77",
   "Е333ЕЕ77",
@@ -124,15 +225,24 @@ const BEAUTIFUL_NUMBERS = [
   "Е777ЕЕ77",
   "Е888ЕЕ77",
   "Е999ЕЕ77",
+
   "Х777ХХ77",
   "Х888ХХ77",
   "Х999ХХ77",
+
   "М777ММ77",
 ].map((plate, i) => ({
   id: `beautiful_${i + 1}`,
   plate,
   price: numberPrice(plate) + 25000,
 }));
+
+
+/*
+=========================================================
+EXPORT
+=========================================================
+*/
 
 module.exports = {
   VEHICLES,
@@ -141,3 +251,4 @@ module.exports = {
   PROPERTY_COLORS,
   BEAUTIFUL_NUMBERS,
   QUICK_PHRASES,
+};
